af971accba58c12e008e0c902a95561debb19bc6
siddhant-afk <siddhantdudhane21@gmail.com>
2023-01-07 17:17:54 +0530

    Add navbar & Create Projects model

---
+++ projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to="images/")
+
+
+
+    def __str__(self):
+        return self.title

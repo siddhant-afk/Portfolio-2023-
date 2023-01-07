bdb57d7115c39bf3b3d035274397cd06904a9419
siddhant-afk <siddhantdudhane21@gmail.com>
2023-01-07 17:23:04 +0530

    Add preview and github URLFields to Projects model

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,8 @@ class Project(models.Model):
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=500)
     image = models.ImageField(upload_to="images/")
+    preview = models.URLField(blank=True)
+    github = models.URLField(blank=True)
 
 
 

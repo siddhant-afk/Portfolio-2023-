b50d77135c6927c13f11aaedcd64bf3ff03185b2
siddhant-afk <siddhantdudhane21@gmail.com>
2023-01-08 19:24:24 +0530

    Remove image field from Projects model

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=500)
-    image = models.ImageField(upload_to="images/")
+    
     preview = models.URLField(blank=True)
     github = models.URLField(blank=True)
 

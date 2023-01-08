ae57723515a168da379a44cd357fac3dd5eaecbc
siddhant-afk <siddhantdudhane21@gmail.com>
2023-01-08 19:20:49 +0530

    Edit project card

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Project
+import datetime as dt
 # Create your views here.
 
+today = dt.date.today()
 
 def home(request):
     projects = Project.objects.all()[::-1]
-    return render(request,"projects/index.html",{'projects' : projects})
+    return render(request,"projects/index.html",{'projects' : projects,'year':today.year})

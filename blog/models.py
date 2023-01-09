fcce9e62567cd7117a6f307c639b710b333b5e57
siddhant-afk <siddhantdudhane21@gmail.com>
2023-01-09 19:22:33 +0530

    Create Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+    link = models.URLField()
+    date = models.DateField()
+
+
+    def __str__(self):
+        return self.title
